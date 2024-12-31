4a919cfa6fe17b1092efc5998e999864b7482048
adina8244 <38214959736@mby.co.il>
2024-12-31 13:17:53 +0200

    change line 8 by adina8244

--- client/src/Components/Todo.jsx
+++ client/src/Components/Todo.jsx
@@ -5,6 +5,7 @@ const Todo=(props)=>{
 
     return(
         <>
+        <p>This is change by adina</p>
             <h1>assignment name:{props.state.id},{props.state.name}</h1>
             <button onClick={()=>{props.dispatch({type:"Delete",what:{id:props.state.id}})}}>Delete</button>
         </>
