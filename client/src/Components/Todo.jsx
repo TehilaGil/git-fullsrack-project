a0fa3f8dffe13ae079b3f28aeb42e92ae6090f90
adina8244 <38214959736@mby.co.il>
2024-12-31 13:24:38 +0200

    change line 13 in todo

--- client/src/Components/Todo.jsx
+++ client/src/Components/Todo.jsx
@@ -7,6 +7,7 @@ const Todo = (props) => {
         <>
             <p>add some words</p>
             <p>This is change by adina</p>
+            <h1>now we try to do conflict in strange places</h1>
             <h1>assignment name:{props.state.id},{props.state.name}</h1>
             <button onClick={() => { props.dispatch({ type: "Delete", what: { id: props.state.id } }) }}>Delete</button>
 
